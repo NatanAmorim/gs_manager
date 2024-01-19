f5735c88610444ac4568bbc6c3029a11d450b838
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2024-01-19 05:31:42 -0300

    Visual improvements on ConfiguracoesSubview

--- lib/src/home/subviews/configuracoes_subview.dart
+++ lib/src/home/subviews/configuracoes_subview.dart
@@ -21,8 +21,8 @@ class ConfiguracoesSubview extends ConsumerWidget {
           children: <TextSpan>[
             TextSpan(
               style: Theme.of(context).textTheme.bodyMedium!,
-              text: 'App de gestão de Gislaine Studio em Andradina.\n'
-                  'Desenvolvido com Flutter, mais informações em\n',
+              text: 'App de gestão de Gislaine Studio em Andradina. '
+                  'Desenvolvido com Flutter, mais informações em ',
             ),
             TextSpan(
               style: Theme.of(context)
@@ -37,176 +37,178 @@ class ConfiguracoesSubview extends ConsumerWidget {
     ];
 
     return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+      padding: const EdgeInsets.all(8.0),
       physics: const BouncingScrollPhysics(),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Flexible(
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(
-                maxWidth: 770,
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 24),
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Configuração Geral",
-                      style: Theme.of(context).textTheme.headlineSmall,
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  Card(
-                    // clipBehavior is necessary because, without it, the InkWell's animation
-                    // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
-                    // This comes with a small performance cost, and you should not set [clipBehavior]
-                    // unless you need it.
-                    clipBehavior: Clip.hardEdge,
-                    elevation: 2.0,
-                    child: ListView(
-                      shrinkWrap: true,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: ListTile.divideTiles(
-                        context: context,
-                        tiles: [
-                          ListTile(
-                            tileColor: Theme.of(context).cardTheme.color!,
-                            title: const Text("Tema do Aplicativo"),
-                            textColor: Theme.of(context).colorScheme.secondary,
-                            leading: const Icon(Icons.palette),
-                            trailing: const Icon(Icons.arrow_right),
-                            iconColor: Theme.of(context).colorScheme.secondary,
-                            onTap: () {
-                              final ThemeMode oldTheme = ref.read(
-                                settingsProvider.select((s) => s.themeMode),
-                              );
-
-                              showDialog(
-                                barrierDismissible: false,
-                                context: context,
-                                builder: (context) => AlertDialog(
-                                  title: const Text("Selecione o tema"),
-                                  content: Scrollbar(
-                                    child: SingleChildScrollView(
-                                      child: Column(
-                                        children: [
-                                          RadioListTile<ThemeMode>(
-                                            title:
-                                                const Text("Tema do sistema"),
-                                            value: ThemeMode.system,
-                                            groupValue: ref.watch(
-                                                settingsProvider.select(
-                                                    (s) => s.themeMode)),
-                                            onChanged: ref
-                                                .read(settingsProvider)
-                                                .updateThemeMode,
-                                          ),
-                                          RadioListTile<ThemeMode>(
-                                            title: const Text("Tema claro"),
-                                            value: ThemeMode.light,
-                                            groupValue: ref.watch(
-                                                settingsProvider.select(
-                                                    (s) => s.themeMode)),
-                                            onChanged: ref
-                                                .read(settingsProvider)
-                                                .updateThemeMode,
-                                          ),
-                                          RadioListTile<ThemeMode>(
-                                            title: const Text("Tema escuro"),
-                                            value: ThemeMode.dark,
-                                            groupValue: ref.watch(
-                                              settingsProvider
-                                                  .select((s) => s.themeMode),
-                                            ),
-                                            onChanged: ref
-                                                .read(settingsProvider)
-                                                .updateThemeMode,
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        if (oldTheme !=
-                                            ref.read(
-                                              settingsProvider
-                                                  .select((s) => s.themeMode),
-                                            )) {
-                                          ref
-                                              .read(settingsProvider)
-                                              .updateThemeMode(oldTheme);
-                                        }
-
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text("CANCELAR"),
-                                    ),
-                                    TextButton(
-                                      onPressed: () =>
-                                          Navigator.of(context).pop(),
-                                      child: const Text("OK"),
-                                    )
-                                  ],
-                                ),
-                              );
-                            },
-                          ),
-                          ListTile(
-                            tileColor: Theme.of(context).cardTheme.color!,
-                            title: const Text("Jurídico e Conformidade"),
-                            textColor: Theme.of(context).colorScheme.secondary,
-                            leading: const Icon(Icons.gavel),
-                            trailing: const Icon(Icons.arrow_right),
-                            iconColor: Theme.of(context).colorScheme.secondary,
-                            onTap: () {
-                              // TODO
-                            },
-                          ),
-                          ListTile(
-                            tileColor: Theme.of(context).cardTheme.color!,
-                            title: const Text("Sobre o App"),
-                            textColor: Theme.of(context).colorScheme.secondary,
-                            leading: const Icon(Icons.info),
-                            trailing: const Icon(Icons.arrow_right),
-                            iconColor: Theme.of(context).colorScheme.secondary,
-                            onTap: () => showAboutDialog(
-                              context: context,
-                              applicationIcon: Image.asset(
-                                'assets/images/gs_manager_logo.png',
-                                width: 128,
-                                height: 128,
-                              ),
-                              applicationName: 'Gislaine Studio Manager App',
-                              applicationVersion: '0.1.0',
-                              applicationLegalese:
-                                  '\u{a9} 2023 Natan Amorim S. G. de Moraes',
-                              children: aboutBoxChildren,
-                            ),
-                          ),
-                          ListTile(
-                            tileColor: Theme.of(context).cardTheme.color!,
-                            title: const Text("Encerrar Sessão"),
-                            textColor: Theme.of(context).colorScheme.secondary,
-                            leading: const Icon(Icons.logout),
-                            trailing: const Icon(Icons.arrow_right),
-                            iconColor: Theme.of(context).colorScheme.secondary,
-                            onTap: () => DialogHelper.logout(
-                              context: context,
-                            ),
-                          ),
-                        ],
-                      ).toList(),
-                    ),
-                  ),
-                ],
+      child: ConstrainedBox(
+        constraints: const BoxConstraints(
+          maxWidth: 786,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 24),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Configurações e Informações",
+                style: Theme.of(context).textTheme.headlineMedium,
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 16),
+            Card(
+              // clipBehavior is necessary because, without it, the InkWell's animation
+              // will extend beyond the rounded edges of the [Card] (see https://github.com/flutter/flutter/issues/109776)
+              // This comes with a small performance cost, and you should not set [clipBehavior]
+              // unless you need it.
+              clipBehavior: Clip.hardEdge,
+              elevation: 4.0,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: ListTile.divideTiles(
+                  context: context,
+                  tiles: [
+                    ListTile(
+                      tileColor: Theme.of(context).cardTheme.color!,
+                      title: const Text("Tema do Aplicativo"),
+                      textColor: Theme.of(context).colorScheme.secondary,
+                      leading: const Icon(Icons.palette),
+                      trailing: const Icon(Icons.arrow_right),
+                      iconColor: Theme.of(context).colorScheme.secondary,
+                      onTap: () {
+                        final ThemeMode oldTheme = ref.read(
+                          settingsProvider.select((s) => s.themeMode),
+                        );
+
+                        showDialog(
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            contentPadding: const EdgeInsets.all(0.0),
+                            title: const Text(
+                              "Selecione o tema do App",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            content: Scrollbar(
+                              child: SingleChildScrollView(
+                                padding: const EdgeInsets.symmetric(
+                                  vertical: 16.0,
+                                ),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    RadioListTile<ThemeMode>(
+                                      title: const Text("Tema do sistema"),
+                                      value: ThemeMode.system,
+                                      groupValue: ref.watch(settingsProvider
+                                          .select((s) => s.themeMode)),
+                                      onChanged: ref
+                                          .read(settingsProvider)
+                                          .updateThemeMode,
+                                    ),
+                                    RadioListTile<ThemeMode>(
+                                      title: const Text("Tema claro"),
+                                      value: ThemeMode.light,
+                                      groupValue: ref.watch(settingsProvider
+                                          .select((s) => s.themeMode)),
+                                      onChanged: ref
+                                          .read(settingsProvider)
+                                          .updateThemeMode,
+                                    ),
+                                    RadioListTile<ThemeMode>(
+                                      title: const Text("Tema escuro"),
+                                      value: ThemeMode.dark,
+                                      groupValue: ref.watch(
+                                        settingsProvider
+                                            .select((s) => s.themeMode),
+                                      ),
+                                      onChanged: ref
+                                          .read(settingsProvider)
+                                          .updateThemeMode,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            actionsAlignment: MainAxisAlignment.spaceBetween,
+                            actions: [
+                              OutlinedButton(
+                                onPressed: () {
+                                  if (oldTheme !=
+                                      ref.read(
+                                        settingsProvider
+                                            .select((s) => s.themeMode),
+                                      )) {
+                                    ref
+                                        .read(settingsProvider)
+                                        .updateThemeMode(oldTheme);
+                                  }
+
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text("CANCELAR"),
+                              ),
+                              FilledButton(
+                                onPressed: () => Navigator.of(context).pop(),
+                                child: const Text("CONFIRMAR"),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      tileColor: Theme.of(context).cardTheme.color!,
+                      title: const Text("Jurídico e Conformidade"),
+                      textColor: Theme.of(context).colorScheme.secondary,
+                      leading: const Icon(Icons.gavel),
+                      trailing: const Icon(Icons.arrow_right),
+                      iconColor: Theme.of(context).colorScheme.secondary,
+                      onTap: () {
+                        // TODO
+                      },
+                    ),
+                    ListTile(
+                      tileColor: Theme.of(context).cardTheme.color!,
+                      title: const Text("Sobre o App"),
+                      textColor: Theme.of(context).colorScheme.secondary,
+                      leading: const Icon(Icons.info),
+                      trailing: const Icon(Icons.arrow_right),
+                      iconColor: Theme.of(context).colorScheme.secondary,
+                      onTap: () => showAboutDialog(
+                        context: context,
+                        applicationIcon: Image.asset(
+                          'assets/images/gs_manager_logo.png',
+                          width: 128,
+                          height: 128,
+                        ),
+                        applicationName: 'Gislaine Studio Manager App',
+                        applicationVersion: '1.0.0',
+                        applicationLegalese:
+                            '\u{a9} 2024 Natan Amorim S. G. de Moraes',
+                        children: aboutBoxChildren,
+                      ),
+                    ),
+                    ListTile(
+                      tileColor: Theme.of(context).cardTheme.color!,
+                      title: const Text("Encerrar Sessão"),
+                      textColor: Theme.of(context).colorScheme.secondary,
+                      leading: const Icon(Icons.logout),
+                      trailing: const Icon(Icons.arrow_right),
+                      iconColor: Theme.of(context).colorScheme.secondary,
+                      onTap: () => DialogHelper.logout(
+                        context: context,
+                      ),
+                    ),
+                  ],
+                ).toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
